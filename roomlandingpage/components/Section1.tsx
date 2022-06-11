575cb2bc8d52510e0d7c54e014f68cb4b8e77f6c
Dradeon <69202556+Dradeon@users.noreply.github.com>
2022-06-11 17:25:06 -0400

    Given Active States to Elements and Did  Styling

--- roomlandingpage/components/Section1.tsx
+++ roomlandingpage/components/Section1.tsx
@@ -53,7 +53,22 @@ const Section1 = () => {
             logo?.setAttribute('style','display:block');
             links?.setAttribute('style','display:none;color:white;')
         }
-    };   
+  };
+  
+  const changeSVGToGray = (e:SyntheticEvent) => {
+    const arrow = document.getElementById('arrow');
+    if(arrow && e){
+        arrow.setAttribute('style','fill:hsl(0,0%,63%);');
+    }
+  }
+
+  const changeSVGToBlack = (e:SyntheticEvent) => {
+    const arrow = document.getElementById('arrow');
+    if(arrow && e){
+        arrow.setAttribute('style','fill:#000;');
+    }
+  }
+
 
   return (
     <div className='flex flex-col w-full sm:flex-row '>
@@ -61,24 +76,41 @@ const Section1 = () => {
             <nav id = 'nav' className='absolute w-[calc(50%+32px)] flex flow-row items-center justify-between py-8 pl-6 custom2:justify-start'>
                 <img id="hamburger" onClick={hamburgerClick} src='icon-hamburger.svg' className='hover:cursor-pointer sm:hidden'></img>
                 <h1 id = 'weblogo' className='font-spartan text-3xl text-White md:mr-12'>room</h1>
-                <ul id = 'links' className='hidden font-spartan text-md text-White md:flex flex-row space-x-8'>
-                    <li><a href='#'>home</a></li>
-                    <li><a href='#'>shop</a></li>
-                    <li><a href='#'>about</a></li>
-                    <li><a href='#'>contact</a></li>
+                <ul id = 'links' className='hidden font-spartan text-md text-White md:flex flex-row flex-nowrap space-x-8 '>
+                    <li className='relative'>
+                        <a href='#' className='pb-2 peer'>home</a>
+                        <div className='absolute top-1/4 left-1/4 w-1/2 h-full mx-auto peer-hover:border-b-2'></div>
+                    </li>
+                    <li className='relative'>
+                        <a href='#' className='pb-2 peer'>shop</a>
+                        <div className='absolute top-1/4 left-1/4 w-1/2 h-full mx-auto peer-hover:border-b-2'></div>
+                    </li>
+                    <li className='relative'>
+                        <a href='#' className='pb-2 peer'>about</a>
+                        <div className='absolute top-1/4 left-1/4 w-1/2 h-full mx-auto peer-hover:border-b-2'></div>
+                    </li>
+                    <li className='relative'>
+                        <a href='#' className='pb-2 peer'>contact</a>
+                        <div className='absolute top-1/4 left-1/4 w-1/2 h-full mx-auto peer-hover:border-b-2'></div>
+                    </li>
                 </ul>
             </nav>
             <div className='absolute flex bottom-0 left-[calc(100%-92px)] custom2:left-full'>
-                <img id='left' src='icon-angle-left.svg' onClick={switchIndex}  className='bg-Black  p-4 hover:cursor-pointer'></img>
-                <img id='right' src='icon-angle-right.svg' onClick={switchIndex} className='bg-Black  p-4 hover:cursor-pointer'></img>
+                <img id='left' src='icon-angle-left.svg' onClick={switchIndex}  className='bg-Black  p-4 hover:cursor-pointer sm:p-8 hover:bg-VeryDarkGray'></img>
+                <img id='right' src='icon-angle-right.svg' onClick={switchIndex} className='bg-Black  p-4 hover:cursor-pointer sm:p-8 hover:bg-VeryDarkGray'></img>
             </div>
             <img id = 'mobilebg' src='mobile-image-hero-1.jpg' className='-z-10 w-full h-[inherit] max-h-[534px] sm:hidden'></img>
             <img id = 'desktopbg' src='desktop-image-hero-1.jpg' className='hidden sm:block -z-10'></img>
         </div>
         <section className='flex flex-col place-content-center space-y-4 p-8 mx-auto max-h-[534px] max-w-lg sm:ml-16 sm:w-1/2'>
-            <h1 className='font-spartan font-bold text-4xl sm:text-5xl'>Discover innovative ways to decorate</h1>
+            <h1 className='font-spartan font-semibold text-4xl sm:text-5xl'>Discover innovative ways to decorate</h1>
             <p className='font-spartan text-sm text-DarkGray sm:text-md'>We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.</p>
-            <a href='#' className='relative w-fit font-spartan tracking-[.75rem]'>SHOP NOW <img src='icon-arrow.svg' className='absolute top-[25%] left-[120%]'></img></a>
+            <a href='#' className='inline-block space-x-4' onMouseOver={changeSVGToGray} onMouseLeave={changeSVGToBlack}>
+                <span className='peer text-sm font-spartan tracking-[.75rem] hover:text-DarkGray'>SHOP NOW</span>
+                <svg width="40" height="12" xmlns="http://www.w3.org/2000/svg"><path id = 'arrow' d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero"/></svg>
+            </a>
+            
+
             
         </section>
     </div>
